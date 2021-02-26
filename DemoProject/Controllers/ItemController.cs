7270dd5461a708db1f1eeca120b83561642fd6ac
Raju Ahmed Shetu <shetu@trustaxiatapay.com>
2021-02-27 01:16:20 +0600

    initialized

---
+++ DemoProject/Controllers/ItemController.cs
@@ -0,0 +1,38 @@
+﻿using DemoProject.Data;
+using DemoProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoProject.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Item> items = _db.Items;   
+            return View(items);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item obj)
+        {
+            _db.Items.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ DemoProject/Models/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoProject.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Who Borrowed")]
+        public string Borrower { get; set; }
+        public string Lender { get; set; }
+        [DisplayName("Item name")]
+        public string ItemName { get; set; }
+    }
+}
